9079d3df3186cefa8301b18591d11fd2c3e6db75
Henrique L S Silva <lsshenrique@hotmail.com>
2019-09-22 16:19:21 -0300

    code removal for better understanding

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,125 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <v-app id="app">
+        <v-content id="vcontent">
+            <v-dialog
+                v-model="dialog"
+                persistent
+                max-width="600px"
+                attach="#vcontent"
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn
+                        color="primary"
+                        dark
+                        v-on="on"
+                    >Open Dialog</v-btn>
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">User Profile</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="4"
+                                >
+                                    <v-text-field
+                                        label="Legal first name*"
+                                        required
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="4"
+                                >
+                                    <v-text-field
+                                        label="Legal middle name"
+                                        hint="example of helper text only on focus"
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="4"
+                                >
+                                    <v-text-field
+                                        label="Legal last name*"
+                                        hint="example of persistent helper text"
+                                        persistent-hint
+                                        required
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-text-field
+                                        label="Email*"
+                                        required
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col cols="12">
+                                    <v-text-field
+                                        label="Password*"
+                                        type="password"
+                                        required
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                >
+                                    <v-select
+                                        :items="['0-17', '18-29', '30-54', '54+']"
+                                        label="Age*"
+                                        required
+                                    ></v-select>
+                                </v-col>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                >
+                                    <v-autocomplete
+                                        :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                                        label="Interests"
+                                        multiple
+                                    ></v-autocomplete>
+                                </v-col>
+                            </v-row>
+                        </v-container>
+                        <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                        <div class="flex-grow-1"></div>
+                        <v-btn
+                            color="blue darken-1"
+                            text
+                            @click="dialog = false"
+                        >Close</v-btn>
+                        <v-btn
+                            color="blue darken-1"
+                            text
+                            @click="dialog = false"
+                        >Save</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify"
 
 export default {
-  name: 'App',
-  vuetify,
-  components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
-};
+    name: "App",
+    vuetify,
+    data: () => ({
+        dialog: false
+    })
+}
 </script>
 <style>
-  @import "../node_modules/vuetify/dist/vuetify.min.css";
+    @import "../node_modules/vuetify/dist/vuetify.min.css";
 </style>
